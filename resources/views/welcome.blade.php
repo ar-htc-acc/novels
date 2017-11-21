5d9a9229255c021a0d03d81da5fd637b5985c580
Louis <ar.htc.acc@gmail.com>
2017-11-21 15:56:24 +0800

    mod

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Louis test site
                 </div>
 
                 <div class="links">
